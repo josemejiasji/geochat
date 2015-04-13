14d2f95cabe58bb67523f8b50248c145cc3dd1e8
Edwin Vasquez <edwinvasquezg@hotmail.com>
2015-04-13 17:07:01 +0200

    Edwin -

--- js/map.js
+++ js/map.js
@@ -1,26 +1,26 @@
-window.onload = function(){
-	if (!navigator.geolocation) {
-		console.log('Geolocation not supported');
-	}
+// window.onload = function(){
+// 	if (!navigator.geolocation) {
+// 		console.log('Geolocation not supported');
+// 	}
 
-	function success(position) {
-		var pos = {};
-		pos.latitude = position.coords.latitude;
-		pos.longitude = position.coords.longitude;
+// 	function success(position) {
+// 		var pos = {};
+// 		pos.latitude = position.coords.latitude;
+// 		pos.longitude = position.coords.longitude;
 
-		return pos;
-	}
+// 		return pos;
+// 	}
 
-	function error (reason) {
-		console.log(error);
-	}
+// 	function error (reason) {
+// 		console.log(error);
+// 	}
 
-	var mapOptions = {
-		position: navigator.geolocation.getCurrentPosition(success, error),
-		center: new google.maps.LatLong(this.position.latitude, this.position.longitude),
-		zoom: 15,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	};
+// 	var mapOptions = {
+// 		position: navigator.geolocation.getCurrentPosition(success, error),
+// 		center: new google.maps.LatLong(this.position.latitude, this.position.longitude),
+// 		zoom: 15,
+// 		mapTypeId: google.maps.MapTypeId.ROADMAP
+// 	};
 
-	var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
-};
+// 	var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+// };
