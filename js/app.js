a091222fe0c117b33064f14718dbe1b18e0091e0
Edwin Vasquez <edwinvasquezg@hotmail.com>
2015-04-14 09:29:02 +0200

    Edwin -

--- js/app.js
+++ js/app.js
@@ -4,23 +4,21 @@
 	angular.module('geoChat', ["firebase","angularGeoFire"]);
 
 	angular.module('geoChat').controller("geoChatController", function($scope, $firebaseObject,$firebaseArray,$geofire) {
-		var ref = new Firebase("https://glaring-heat-1935.firebaseio.com");
-		var ubicacion = new Firebase("https://glaring-heat-1935.firebaseio.com/ubicacion");
-		var obj = $firebaseObject(ubicacion);
+		var ref = new Firebase("https://glaring-heat-1935.firebaseio.com"),
+		    historial = new Firebase("https://glaring-heat-1935.firebaseio.com/historial"),
+				obj = $firebaseObject(historial),
+				f=new Date(),
+				post={
+					latitude:465,
+					longitude:045,
+					mensaje:{
+						user:"",
+						texto:"",
+						fecha:f.toDateString()
+					}
+				};
 
 		$scope.mensajes=[];
-		$scope.user ="Edwin";
-
-		var post={
-			latitude:465,
-			longitude:045,
-			mensaje:{
-				user:"",
-				texto:"",
-				fecha:new Date()
-			}
-
-		};
 
 		//Aqui vamos recogiendo los valores
 		obj.$watch(function(){
@@ -38,9 +36,7 @@
 			//Creamos la cordenada en la base de datos
 			post.mensaje.user=$scope.user;
 			post.mensaje.texto=$scope.message;
-			ubicacion.push(post);
-
+			historial.push(post);
 		}
-
 
 	});
