8d6df3464b32002ceff066a0b9e0341d9fa3fda5
Edwin Vasquez <edwinvasquezg@hotmail.com>
2015-04-14 09:10:32 +0200

    Edwin -

--- js/app.js
+++ js/app.js
@@ -7,8 +7,10 @@
 		var ref = new Firebase("https://glaring-heat-1935.firebaseio.com");
 		var ubicacion = new Firebase("https://glaring-heat-1935.firebaseio.com/ubicacion");
 		var obj = $firebaseObject(ubicacion);
+
 		$scope.mensajes=[];
 		$scope.user ="Edwin";
+
 		var post={
 			latitude:465,
 			longitude:045,
@@ -20,19 +22,11 @@
 
 		};
 
-		obj.$loaded().then(function() {
-        console.log("loaded record:", obj.$id, obj.someOtherKeyInData);
-
-       // To iterate the key/value pairs of the object, use angular.forEach()
-       angular.forEach(obj, function(value, key) {
-          console.log(key, value);
-
-       });
-     });
-
 		//Aqui vamos recogiendo los valores
 		obj.$watch(function(){
+			//Hay que filtral por cordenadas
 			$scope.mensajes = [];
+
 			angular.forEach(obj, function(value, key) {
 					//$scope.mensajes.;
           console.log(key, value);
@@ -46,12 +40,6 @@
 			post.mensaje.texto=$scope.message;
 			ubicacion.push(post);
 
-			//Se debe de pillar la ubicacion en la que se hizo el mensaje
-			// ref.push({
-			// 	cordenadas:[0,0],
-			// 	user:$scope.user,
-			// 	mensaje:$scope.message
-			// })
 		}
 
 
